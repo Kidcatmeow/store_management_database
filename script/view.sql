1429ea104040bbb6955e7ea095716dd48538bdf7
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2023-06-12 00:31:16 +0700

    "view for total_sales fixed(2)"

--- script/view.sql
+++ script/view.sql
@@ -1,5 +1,3 @@
-SET SESSION sql_mode='';
-
 CREATE VIEW item_sales_view AS
 SELECT a.stock_id,b.item_name,a.price,a.quantity, SUM(a.quantity * a.price) AS total_sales
 FROM orderitem AS a,stock AS b
