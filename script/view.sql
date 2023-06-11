d71060cf6d484583fdc8befcaa01f6ed2a6984df
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2023-06-12 00:24:49 +0700

    total sales view added

---
+++ script/view.sql
@@ -0,0 +1,8 @@
+SET SESSION sql_mode='';
+
+CREATE VIEW item_sales_view AS
+SELECT a.stock_id,b.item_name,a.price,a.quantity, SUM(a.quantity * a.price) AS total_sales
+FROM orderitem AS a,stock AS b
+WHERE a.stock_id = b.stock_id
+GROUP BY stock_id
+ORDER BY total_sales DESC;
