9ac0f29fbb2167e59bb850ac45573902e875a7e5
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2023-06-12 00:29:58 +0700

    "view for total_sales fixed"

--- script/view.sql
+++ script/view.sql
@@ -4,5 +4,7 @@ CREATE VIEW item_sales_view AS
 SELECT a.stock_id,b.item_name,a.price,a.quantity, SUM(a.quantity * a.price) AS total_sales
 FROM orderitem AS a,stock AS b
 WHERE a.stock_id = b.stock_id
-GROUP BY stock_id
+GROUP BY stock_id,item_name,price,quantity
 ORDER BY total_sales DESC;
+
+SELECT * FROM item_sales_view;
